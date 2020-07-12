fe93f702a4cf2552b23867293d82af54176d4d84
exthitikorn <rockerzeeza@gmail.com>
2020-07-12 15:52:37 +0700

    Finaly GGWP

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
+import postgresql_api
+
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
@@ -26,7 +29,12 @@ def sum_faction():
 @app.route('/mypage')
 def mypage_faction():
     username = request.args.get('name')
-    return render_template('home.html',name= username)
+    return render_template('home.html', name = username)
+
+@app.route('/studentlist')
+def student_list_faction():
+    stdlist = postgresql_api.get_student_data()
+    return render_template('table.html', stdlist = stdlist)
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5000)
--- postgresql_api.py
+++ postgresql_api.py
@@ -1,8 +1,9 @@
 import psycopg2
 import os
+
 try:
     # db_secret = os.environ["DATABASE_URL"]
-    db_secret = ''
+    db_secret = 'postgres://xanrjxwnnjfcat:ee47a4bf184c8177c5f3b943397a21a120628c11d472d737c11112d5a4cc05b0@ec2-52-200-48-116.compute-1.amazonaws.com:5432/d3jno2a52ealhp'
     connection = psycopg2.connect(db_secret)
     connection.set_session(autocommit=True)
 
@@ -22,5 +23,17 @@ try:
 except (Exception, psycopg2.Error) as error :
     print ("Error while connecting to PostgreSQL", error)
 
-def get_example_data():
-    pass
+def get_student_data():
+    cur = connection.cursor()
+    cur.execute("""
+    select firstname,lastname,age,gpa
+    from student;
+    """)
+    rows = cur.fetchall()
+    print('Student :')
+    print(rows)
+    cur.close()
+    return rows
+
+
+get_student_data()
