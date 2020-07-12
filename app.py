241af5498e49683bd1eb407a5d3abc771fea0087
exthitikorn <rockerzeeza@gmail.com>
2020-07-12 13:33:24 +0700

    add home and fix app.py

--- app.py
+++ app.py
@@ -7,7 +7,26 @@ CORS(app)
 
 @app.route('/')
 def root():
-    return "Working"
+    return "สวัสดีวันอาทิตย์"
+
+@app.route('/new')
+def new_one_faction():
+    username = request.args.get('name')
+    print(username)
+    return 'Hi ' + username
+
+@app.route('/sum')
+def sum_faction():
+    a = request.args.get('a')
+    a = int(a)
+    b = int(request.args.get('b'))
+    ans = a + b
+    return str(ans)
+
+@app.route('/mypage')
+def mypage_faction():
+    username = request.args.get('name')
+    return render_template('home.html',name= username)
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5000)
